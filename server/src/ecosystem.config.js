aded0e956af442b05bf07b9719e49ea8ffc98267
LeandroF01 <leaylio3327@gmail.com>
2023-04-11 15:12:54 -0300

    Add system server of pm2

---
+++ server/src/ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	apps: [
+		{
+			name: "index",
+			script: "./index.js",
+			watch: true,
+			env: {
+				NODE_ENV: "development",
+			},
+			env_production: {
+				NODE_ENV: "production",
+			},
+		},
+	],
+};
